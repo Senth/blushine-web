6151edec350582f99d0f1b91acee31b4145e4f98
Matteus Magnusson <senth.wallace@gmail.com>
2021-03-22 18:16:45 +0100

    🐛 Stripper uses interval 1 tick

--- backend/src/app/command/player/CommandPlayerFactory.ts
+++ backend/src/app/command/player/CommandPlayerFactory.ts
@@ -315,7 +315,7 @@ class PlayerStripper extends PlayerCommand {
   }
 
   start(): string[] {
-    return [`/player ${this.player} use continuous`]
+    return [`/player ${this.player} use interval 1`]
   }
 
   stop(): string[] {
